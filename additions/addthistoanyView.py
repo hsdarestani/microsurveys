4545e1ca36490c05931c45d7300bc92584a92af7
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2023-01-16 14:17:49 +0330

    Update addthistoanyView.py

--- additions/addthistoanyView.py
+++ additions/addthistoanyView.py
@@ -12,7 +12,7 @@
         ansHtml=""
         ansemojies = ["😑","🙂","😊","😍"]
         for a in range(MinAns,MaxAns+1):
-            ansHtml+='<input  type="radio" id="a'+str(a)+'" name="'+str(e.id)+'" value="'+str(a)+'" onclick="sendFeedback('+str(e.id)+','+str(a)+','+str(evcounter)+')"/><label  for="a'+str(a)+'">'+ansemojies[a]+'</label>'
+            ansHtml+='<input  type="radio" id="a'+str(a)+'" name="'+str(e.id)+'" value="'+str(a)+'" onclick="sendFeedback('+str(e.id)+','+str(a)+','+str(evcounter)+')"/><label  for="a'+str(a)+'">'+ansemojies[a-MinAns]+'</label>'
         html = html.replace("%choices%",ansHtml)
         scr = "<script> span"+str(evcounter)+"=document.getElementsByClassName('micclose')["+str(evcounter)+"]; span"+str(evcounter)+".onclick = function() {document.getElementById('mic"+str(evcounter)+"').style.display = 'none';};setTimeout(function() {document.getElementById('mic"+str(evcounter)+"').style.display='block';}, "+str(delay)+");</script>"
         html+=scr
